18ee17c2ef50681e93935777f5c756d21ad5c8fc
rafal <trzcinskiraf@gmail.com>
2016-01-28 14:00:22 +0100

    removed unused code

--- app/src/test/java/pl/elpassion/cloudtimer/ExampleUnitTest.java
+++
@@ -1,9 +0,0 @@
-package pl.elpassion.cloudtimer;
-
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
-public class ExampleUnitTest {
-    public void addition_isCorrect() throws Exception {
-    }
-}
