e13bc8171cd0f107272d31ba1396bfd6367b16b7
rafal <trzcinskiraf@gmail.com>
2016-01-28 16:49:09 +0100

    change ApplicationTest to Kotlin

---
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package pl.elpassion.cloudtimer
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
