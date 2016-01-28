83988fbaa7a373611fd0cc45fbba6289d02e00d8
rafal <trzcinskiraf@gmail.com>
2016-01-28 14:42:48 +0100

    change tests to JUnit4

--- app/src/androidTest/java/pl/elpassion/cloudtimer/SimpleTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/SimpleTest.kt
@@ -4,21 +4,22 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
-import android.test.ActivityInstrumentationTestCase2
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 
-class SimpleTest : ActivityInstrumentationTestCase2<MainActivity>(MainActivity::class.java){
+@RunWith(AndroidJUnit4::class)
+class SimpleTest {
 
-    override fun setUp() {
-        super.setUp()
-        activity
-    }
+    @Rule @JvmField
+    public val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
-    fun testCheckIfHelloWorldIsPresent() {
+
+    @Test
+    fun checkIfHelloWorldIsPresent() {
         onView(withId(R.id.hello_world_text)).check(matches(withText("Hello World!")))
-    }
-
-    fun testWhichShouldNotPass() {
-        onView(withId(R.id.hello_world_text)).check(matches(withText("Hello !")))
     }
 }
