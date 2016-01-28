6b3a0b5e90ff031a7b6b663330a58f5d77a11a64
rafal <trzcinskiraf@gmail.com>
2016-01-28 13:05:01 +0100

    add testing support: simple test for Hello World, travis running test suit

---
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/SimpleTest.kt
@@ -0,0 +1,20 @@
+package pl.elpassion.cloudtimer
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.test.ActivityInstrumentationTestCase2
+
+
+class SimpleTest : ActivityInstrumentationTestCase2<MainActivity>(MainActivity::class.java){
+
+    override fun setUp() {
+        super.setUp()
+        activity
+    }
+
+    fun testCheckIfHelloWorldIsPresent() {
+        onView(withId(R.id.hello_world_text)).check(matches(withText("Hello World!")))
+    }
+}
