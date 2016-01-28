5246cce329e7afc311ef869eaeface7952f17a07
Kit <s10474@pjwstk.edu.pl>
2016-01-28 12:28:30 +0100

    refactoring: java to kotlin

---
+++ app/src/main/java/pl/elpassion/cloudtimer/MainActivity.kt
@@ -0,0 +1,19 @@
+package pl.elpassion.cloudtimer
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
