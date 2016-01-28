d056d52d6b1e14cb119f7a7a54763e4ad89c2c80
rafal <trzcinskiraf@gmail.com>
2016-01-28 17:13:24 +0100

    remove ApplicationTest

--- app/src/androidTest/java/pl/elpassion/cloudtimer/ApplicationTest.kt
+++
@@ -1,6 +0,0 @@
-package pl.elpassion.cloudtimer
-
-import android.app.Application
-import android.test.ApplicationTestCase
-
-class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
