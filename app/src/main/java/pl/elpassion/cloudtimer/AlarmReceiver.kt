57d2da80223d39dc5527af5d6f64c82225191120
Kit <s10474@pjwstk.edu.pl>
2016-02-02 11:29:25 +0100

    reciever added #13

---
+++ app/src/main/java/pl/elpassion/cloudtimer/AlarmReceiver.kt
@@ -0,0 +1,19 @@
+package pl.elpassion.cloudtimer
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+
+class AlarmReceiver: BroadcastReceiver() {
+    companion object {
+        public final val REQUEST_CODE = 424232
+        public final val ACTION_STRING = "pl.elpassion.cloudtimer.local.alarm"
+    }
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // todo implementation of receiver
+        Log.e("ALARM", "broadcast recieved")
+    }
+
+}
