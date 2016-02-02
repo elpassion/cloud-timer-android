e5c33184b50cd7e4a1dc9195d58dc57bdacd7de7
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-02 15:55:45 +0100

    refactoring [#12]

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-./gradlew connectedCheck
 mkdir -p app/build/reports/jacoco/test/
 cp app/build/reports/coverage/debug/report.xml app/build/reports/jacoco/test/jacocoTestReport.xml
-./gradlew crashlyticsUploadDistributionDebug
+./gradlew coveralls
