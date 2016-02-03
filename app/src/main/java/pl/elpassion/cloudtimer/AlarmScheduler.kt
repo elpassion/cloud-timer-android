9540da392c341bf14c1990f370ea54c3325e65c6
Kit <s10474@pjwstk.edu.pl>
2016-02-03 13:49:00 +0100

    set/setExact used depending on system version #13

--- app/src/main/java/pl/elpassion/cloudtimer/AlarmScheduler.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/AlarmScheduler.kt
@@ -12,6 +12,11 @@ public fun scheduleAlarm(timer : AlarmData, context : Context) {
             intent, PendingIntent.FLAG_UPDATE_CURRENT)
     val alarmTime = System.currentTimeMillis() + timer.timeInMillis
     val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    alarmManager.setAlarmClock(AlarmManager.AlarmClockInfo(alarmTime, pendingIntent), pendingIntent)
+
+    if (android.os.Build.VERSION.SDK_INT >= 19) {
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent)
+    } else {
+        alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent)
+    }
     Log.e("ALARM", "SYS TIME MILIS: "+ System.currentTimeMillis())
 }
