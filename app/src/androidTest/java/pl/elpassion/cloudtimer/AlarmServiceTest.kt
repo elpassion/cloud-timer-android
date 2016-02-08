986f254e7b52caa9d9e17e45099e2e1f38abd925
Kit <s10474@pjwstk.edu.pl>
2016-02-08 10:31:29 +0100

    #13 added tests for reciever/ scheduler/ media player

--- app/src/androidTest/java/pl/elpassion/cloudtimer/AlarmServiceTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/AlarmServiceTest.kt
@@ -3,69 +3,76 @@ package pl.elpassion.cloudtimer
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.support.test.rule.ActivityTestRule
+import android.os.SystemClock
 import android.support.test.runner.AndroidJUnit4
+import android.test.ActivityInstrumentationTestCase2
 import org.junit.Assert
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.pressButton
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
-class AlarmServiceTest {
-    @Rule @JvmField
-    public val activity = ActivityTestRule<TimerActivity>(TimerActivity::class.java)
-
-    private val context: Context by lazy { activity.activity }
+class AlarmServiceTest : ActivityInstrumentationTestCase2<MainActivity>(MainActivity::class.java) {
 
     @Test
     fun alarmSchedulerNoPendingIntentsAtStart()
     {
+        activity
         clearIntent()
         val alarmUp = (PendingIntent.getBroadcast
-        (context, AlarmReceiver.REQUEST_CODE,
-                Intent(context, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE))
+        (activity, AlarmReceiver.REQUEST_CODE,
+                Intent(activity, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE))
         Assert.assertTrue(alarmUp == null)
     }
 
     @Test
     fun alarmSchedulerCreatesPendingIntent()
     {
+        activity
         clearIntent()
         pressButton(R.id.start_button)
         val alarmUp = (PendingIntent.getBroadcast
-                (context, AlarmReceiver.REQUEST_CODE,
-                        Intent(context, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE))
+                (activity, AlarmReceiver.REQUEST_CODE,
+                        Intent(activity, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE))
         Assert.assertTrue(alarmUp != null)
     }
 
     @Test
     fun alarmSchedulerNoRandomIntentType()
     {
+        activity
         clearIntent()
         val requestCodeToTry = 123
         pressButton(R.id.start_button)
         val alarmUp = (PendingIntent.getBroadcast
-        (context, requestCodeToTry,
-                Intent(context, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE))
+        (activity, requestCodeToTry,
+                Intent(activity, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE))
         Assert.assertTrue(alarmUp == null)
     }
 
     fun clearIntent()
     {
-        val intent = PendingIntent.getBroadcast(context, AlarmReceiver.REQUEST_CODE,
-                Intent(context, AlarmReceiver::class.java), PendingIntent.FLAG_CANCEL_CURRENT)
+        val intent = PendingIntent.getBroadcast(activity, AlarmReceiver.REQUEST_CODE,
+                Intent(activity, AlarmReceiver::class.java), PendingIntent.FLAG_CANCEL_CURRENT)
         intent.cancel()
     }
 
     @Test
-    fun serviceReaction()
+    fun alarmReceived()
     {
-       // val service = AlarmReceiver()
-        val i = Intent(context, AlarmReceiver::class.java)
-        context!!.sendBroadcast(i)
-      //  service.onReceive(context, i)
-        // todo
-        //Assert.assertTrue()
+        var alarmRecived = false
+        soundPlayer = { context: Context ->
+            alarmRecived = true
+        }
+        activity
+
+        val newTimer = AlarmData(TimeUnit.MILLISECONDS.convert(1, TimeUnit.SECONDS))
+        scheduleAlarm(newTimer, activity)
+        //tod
+
+        SystemClock.sleep(2000)
+
+        Assert.assertTrue(alarmRecived)
     }
 }
--- app/src/main/java/pl/elpassion/cloudtimer/AlarmReceiver.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/AlarmReceiver.kt
@@ -3,6 +3,9 @@ package pl.elpassion.cloudtimer
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.util.Log
 
 
@@ -13,8 +16,18 @@ class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.e("ALARM", "broadcast recieved")
-        val i = Intent(context, AlarmService::class.java)
-        context.startService(i)
+        soundPlayer(context)
     }
-
 }
+
+var soundPlayer =
+     fun (context : Context) {
+        val player = MediaPlayer()
+        player.setAudioStreamType(AudioManager.STREAM_ALARM)
+        player.setDataSource(context,
+                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+        player.prepare()
+        player.start()
+        Log.e("ALARM", "ram pam pam")
+        player.setOnCompletionListener { mediaPlayer -> mediaPlayer.release() }
+    }
