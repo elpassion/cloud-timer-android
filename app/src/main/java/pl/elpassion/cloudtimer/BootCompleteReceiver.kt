461553533fc3d7c98edd5d7cec8114e176aa2b4f
rafal <trzcinskiraf@gmail.com>
2016-02-10 12:21:25 +0100

    add activate notification after restart device when timer ends

---
+++ app/src/main/java/pl/elpassion/cloudtimer/BootCompleteReceiver.kt
@@ -0,0 +1,25 @@
+package pl.elpassion.cloudtimer
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import pl.elpassion.cloudtimer.alarm.scheduleAlarm
+import pl.elpassion.cloudtimer.domain.Timer
+
+
+class BootCompleteReceiver : BroadcastReceiver() {
+
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        Log.e("BOOT COMPLETED", "Device is started")
+        val currentTime = System.currentTimeMillis()
+        val timerDao = TimerDAO.getInstance(context)
+        val timers = timerDao.findAll()
+        scheduleAlarmForActiveTimers(context, currentTime, timers)
+    }
+
+    private fun scheduleAlarmForActiveTimers(context: Context, currentTime: Long, timers: List<Timer>) {
+        timers.filter { it.endTime > currentTime }.forEach { scheduleAlarm(it, context) }
+    }
+}
