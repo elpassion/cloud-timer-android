ab99a6cd86b2bc3cd683abed0c00402e8ad931b4
Kit <s10474@pjwstk.edu.pl>
2016-02-10 17:07:11 +0100

    #17 refactor

--- app/src/androidTest/java/pl/elpassion/cloudtimer/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/LoginActivityTest.kt
@@ -1,9 +1,7 @@
 package pl.elpassion.cloudtimer
 
-import android.os.SystemClock
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.test.ActivityInstrumentationTestCase2
 import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
@@ -16,13 +14,12 @@ import pl.elpassion.cloudtimer.timerslist.ListOfTimersActivity
 @RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
     @Rule @JvmField
-    val activity = ActivityTestRule<ListOfTimersActivity>(ListOfTimersActivity::class.java)
+    val rule = ActivityTestRule<ListOfTimersActivity>(ListOfTimersActivity::class.java)
 
     companion object {
-        val alarmDao by lazy { TimerDAO.getInstance() }
-
         @BeforeClass @JvmStatic
-        fun a() {
+        fun daoSetup() {
+            val alarmDao = TimerDAO.getInstance()
             alarmDao.deleteAll()
             alarmDao.save(Timer("placeholder", 100L))
         }
@@ -31,7 +28,6 @@ class LoginActivityTest {
     @Test
     fun startLoginActivity() {
         pressButton(R.id.timer_share_button)
-        SystemClock.sleep(100)
         isComponentDisplayed(R.id.login_via_email_button)
     }
 }
