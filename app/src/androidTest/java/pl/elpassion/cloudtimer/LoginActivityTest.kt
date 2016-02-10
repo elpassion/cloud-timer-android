ad791bbd9c112dca5187ec1e9e3937ec2fb4bc0e
Kit <s10474@pjwstk.edu.pl>
2016-02-10 16:58:48 +0100

    #17 test fix

--- app/src/androidTest/java/pl/elpassion/cloudtimer/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/LoginActivityTest.kt
@@ -1,9 +1,11 @@
 package pl.elpassion.cloudtimer
 
 import android.os.SystemClock
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.test.ActivityInstrumentationTestCase2
 import org.junit.BeforeClass
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.isComponentDisplayed
@@ -12,12 +14,14 @@ import pl.elpassion.cloudtimer.domain.Timer
 import pl.elpassion.cloudtimer.timerslist.ListOfTimersActivity
 
 @RunWith(AndroidJUnit4::class)
-class LoginActivityTest : ActivityInstrumentationTestCase2<ListOfTimersActivity>(ListOfTimersActivity::class.java) {
+class LoginActivityTest {
+    @Rule @JvmField
+    val activity = ActivityTestRule<ListOfTimersActivity>(ListOfTimersActivity::class.java)
 
     companion object {
         val alarmDao by lazy { TimerDAO.getInstance() }
 
-        @BeforeClass
+        @BeforeClass @JvmStatic
         fun a() {
             alarmDao.deleteAll()
             alarmDao.save(Timer("placeholder", 100L))
@@ -26,10 +30,8 @@ class LoginActivityTest : ActivityInstrumentationTestCase2<ListOfTimersActivity>
 
     @Test
     fun startLoginActivity() {
-        activity
         pressButton(R.id.timer_share_button)
         SystemClock.sleep(100)
         isComponentDisplayed(R.id.login_via_email_button)
     }
-
 }
