7c0e6b871e5da007e9556a4594490cad0447305d
rafal <trzcinskiraf@gmail.com>
2016-02-11 10:23:25 +0100

    change CreateNewTimerTest: clear table first and add one timer

--- app/src/androidTest/java/pl/elpassion/cloudtimer/CreateNewTimerTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/CreateNewTimerTest.kt
@@ -2,11 +2,13 @@ package pl.elpassion.cloudtimer
 
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.isComponentDisplayed
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.pressButton
+import pl.elpassion.cloudtimer.domain.Timer
 import pl.elpassion.cloudtimer.timerslist.ListOfTimersActivity
 
 @RunWith(AndroidJUnit4::class)
@@ -14,6 +16,15 @@ class CreateNewTimerTest {
 
     @Rule @JvmField
     val listOfTimersActivity = ActivityTestRule<ListOfTimersActivity>(ListOfTimersActivity::class.java)
+
+    @Before
+    fun setUpTimer() {
+        val timerDao = TimerDAO.getInstance(listOfTimersActivity.activity)
+        timerDao.deleteAll()
+        timerDao.save(Timer(
+                title = "TestTimer",
+                duration = 35000))
+    }
 
     @Test
     fun isTimerActivityStartsWhenAddNewTimerButtonIsPushed() {
