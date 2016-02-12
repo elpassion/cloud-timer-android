743123cb62d0cecbc048e8e4fc0981ac83c2bae8
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-12 16:03:23 +0100

    refactoring

--- app/src/androidTest/java/pl/elpassion/cloudtimer/timer/TimersListActionsTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/timer/TimersListActionsTest.kt
@@ -2,6 +2,7 @@ package pl.elpassion.cloudtimer.timerslist
 
 import android.support.v7.widget.RecyclerView
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import pl.elpassion.cloudtimer.domain.Timer
 import java.util.*
@@ -9,30 +10,27 @@ import java.util.*
 
 class TimersListActionsTest {
 
+    val adapter = NewAdapter()
+    val operations = ArrayList<Pair<Int, Int>>()
+    val observer = object : RecyclerView.AdapterDataObserver() {
+        override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+            operations.add(Pair(positionStart, itemCount))
+        }
+    }
+
+    @Before
+    fun registerObserver() {
+        adapter.registerAdapterDataObserver(observer)
+    }
+
     @Test
     fun whenSingleItemIsAddedNotifyItemOnPositionZeroInserted() {
-        val operations = ArrayList<Pair<Int, Int>>()
-        val adapter = NewAdapter()
-        val observer = object : RecyclerView.AdapterDataObserver() {
-            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                operations.add(Pair(positionStart, itemCount))
-            }
-        }
-        adapter.registerAdapterDataObserver(observer)
         adapter.updateTimers(listOf(Timer("test", 1000)))
         assertEquals(listOf(Pair(0, 1)), operations)
     }
 
     @Test
     fun whenTwoItemsAreAddedNotifyItemsOnPositionZeroAndOneInserted() {
-        val operations = ArrayList<Pair<Int, Int>>()
-        val adapter = NewAdapter()
-        val observer = object : RecyclerView.AdapterDataObserver() {
-            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                operations.add(Pair(positionStart, itemCount))
-            }
-        }
-        adapter.registerAdapterDataObserver(observer)
         adapter.updateTimers(listOf(Timer("test", 1000), Timer("test2", 2000)))
         assertEquals(listOf(Pair(0, 2)), operations)
     }
