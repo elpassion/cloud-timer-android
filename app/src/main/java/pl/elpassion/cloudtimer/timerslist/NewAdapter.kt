edd17dee5be3bc7671d3ee86301613db076a8efc
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-12 16:45:22 +0100

    simplification of implementation

--- app/src/main/java/pl/elpassion/cloudtimer/timerslist/NewAdapter.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/timerslist/NewAdapter.kt
@@ -5,19 +5,9 @@ import pl.elpassion.cloudtimer.domain.Timer
 
 class NewAdapter : BaseAdapter() {
     fun updateTimers(timers: List<Timer>) {
-        if (adapters.size > 0) {
-            if (adapters.size < timers.size) {
-                notifyItemRangeInserted(0, timers.size - adapters.size)
-                notifyItemRangeChanged(timers.size - adapters.size, adapters.size)
-                adapters.clear()
-                adapters.addAll(createAdaptersForCloudTimerItems(timers))
-                return
-            } else {
-                notifyItemRangeChanged(0, timers.size)
-                return
-            }
-        }
+        notifyItemRangeInserted(0, timers.size - adapters.size)
+        notifyItemRangeChanged(timers.size - adapters.size, adapters.size)
+        adapters.clear()
         adapters.addAll(createAdaptersForCloudTimerItems(timers))
-        notifyItemRangeInserted(0, timers.size)
     }
 }
