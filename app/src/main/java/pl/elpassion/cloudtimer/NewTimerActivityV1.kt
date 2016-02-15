00f4841a34486032eb83287ed42d580c90d1059e
rafal <trzcinskiraf@gmail.com>
2016-02-15 14:44:48 +0100

    Change design for timer activity #29

--- app/src/main/java/pl/elpassion/cloudtimer/NewTimerActivityV1.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/NewTimerActivityV1.kt
@@ -6,11 +6,10 @@ import android.widget.TextView
 import com.triggertrap.seekarc.SeekArc
 
 
-class NewTimerActivityV1 : Activity(){
+class NewTimerActivityV1 : Activity() {
 
     val seekArcMinutes by lazy { findViewById(R.id.timer_seekArc_minutes) as SeekArc }
-    val timerDurationMinutes by lazy { findViewById(R.id.timer_duration_minutes) as TextView }
-    val timerDurationSeconds by lazy { findViewById(R.id.timer_duration_seconds) as TextView }
+    val timerDuration by lazy { findViewById(R.id.timer_duration) as TextView }
     val timerTimeToEnd by lazy { findViewById(R.id.timer_time_to_end) as TextView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +20,31 @@ class NewTimerActivityV1 : Activity(){
 
     inner class SeekArcMinutesChangeListener : SeekArc.OnSeekArcChangeListener {
 
-        override fun onProgressChanged(p0: SeekArc?, currentValue: Int, p2: Boolean) {
+        var previousTimerValue: Int = 0
+        var hoursValue : Int = 0
+        val beginRange = 0..10
+        val endRange = 50..59
 
-            val currentTimerValue = Math.round(currentValue/360f*59)
-            timerDurationMinutes.text = currentTimerValue.toString()
-            val timerDurationInMilis = currentTimerValue * 60 * 1000
+        override fun onProgressChanged(seekArc: SeekArc?, currentValue: Int, isChanged: Boolean) {
+
             val currentTime = System.currentTimeMillis()
-            timerDurationSeconds.text = ":00"
-            timerTimeToEnd.text = TimeConverter.formatFromMilliToTime(currentTime + timerDurationInMilis)
+            val currentTimerValue = Math.round(currentValue / 360f * 59)
+            val currentTimerValueInMilis = currentTimerValue * 60 * 1000
+
+            if (currentTimerValue in beginRange && previousTimerValue.toInt() in endRange) {
+                hoursValue++
+            } else if (previousTimerValue in beginRange && currentTimerValue in endRange) {
+                hoursValue--
+                if (hoursValue < 0) {
+                    hoursValue = 0
+                }
+            }
+
+            val durationInMilis = (hoursValue * 60 * 60 * 1000) + currentTimerValueInMilis
+            val timerEndTimeInMilis = currentTime + durationInMilis
+            timerTimeToEnd.text = TimeConverter.formatFromMilliToTime(timerEndTimeInMilis)
+            timerDuration.text = TimeConverter.formatFromMilliToMinutes(durationInMilis.toLong())
+            previousTimerValue = currentTimerValue
         }
 
         override fun onStartTrackingTouch(p0: SeekArc?) {
--- app/src/main/java/pl/elpassion/cloudtimer/TimerActivity.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/TimerActivity.kt
@@ -4,30 +4,27 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.NumberPicker
+import android.widget.TextView
+import com.triggertrap.seekarc.SeekArc
 import pl.elpassion.cloudtimer.alarm.scheduleAlarm
 import pl.elpassion.cloudtimer.domain.Timer
 
+
 class TimerActivity : Activity() {
-    val hoursPicker by lazy { findViewById(R.id.hours_picker)as NumberPicker }
-    val minutesPicker by lazy { findViewById(R.id.minutes_picker) as NumberPicker }
-    val secondsPicker by lazy { findViewById(R.id.seconds_picker) as NumberPicker }
-    val startButton by lazy { findViewById(R.id.start_button) as Button }
-    val timerTitle by lazy { findViewById(R.id.timer_title) as EditText }
+
+    val seekArcMinutes by lazy { findViewById(R.id.timer_seekArc_minutes) as SeekArc }
+    val timerDuration by lazy { findViewById(R.id.timer_duration) as TextView }
+    val timerTimeToEnd by lazy { findViewById(R.id.timer_time_to_end) as TextView }
+    val startTimerButton by lazy { findViewById(R.id.start_timer_button) as Button }
+    val timerTitle by lazy { findViewById(R.id.new_timer_title) as EditText }
     protected val alarmDao by lazy { TimerDAO.getInstance(applicationContext) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
-        hoursPicker.setValues(0, 24, 0)
-        minutesPicker.setValues(0, 59, 15)
-        secondsPicker.setValues(0, 59, 0)
-    }
+        seekArcMinutes.setOnSeekArcChangeListener(SeekArcMinutesChangeListener())
 
-    override fun onStart() {
-        super.onStart()
-        startButton.setOnClickListener {
-            // TODO: TMP create timer
+        startTimerButton.setOnClickListener {
             val newTimer = Timer(timerTitle.text.toString(), getTime())
             scheduleAlarm(newTimer, this)
             alarmDao.save(newTimer)
@@ -35,14 +32,44 @@ class TimerActivity : Activity() {
         }
     }
 
-    private fun NumberPicker.setValues(minVal: Int, maxVal: Int, defaultValue: Int) {
-        minValue = minVal
-        maxValue = maxVal
-        wrapSelectorWheel = true
-        value = defaultValue
+    private fun getTime() : Long {
+        return convertTime(12, 50, 11)
     }
 
-    private fun getTime(): Long {
-        return convertTime(hoursPicker.value, minutesPicker.value, secondsPicker.value)
+    inner class SeekArcMinutesChangeListener : SeekArc.OnSeekArcChangeListener {
+
+        var previousTimerValue: Int = 0
+        var hoursValue: Int = 0
+        val beginRange = 0..10
+        val endRange = 50..59
+
+        override fun onProgressChanged(seekArc: SeekArc?, currentValue: Int, isChanged: Boolean) {
+
+            val currentTime = System.currentTimeMillis()
+            val currentTimerValue = Math.round(currentValue / 360f * 59)
+            val currentTimerValueInMilis = currentTimerValue * 60 * 1000
+
+            if (currentTimerValue in beginRange && previousTimerValue.toInt() in endRange) {
+                hoursValue++
+            } else if (previousTimerValue in beginRange && currentTimerValue in endRange) {
+                hoursValue--
+                if (hoursValue < 0) {
+                    hoursValue = 0
+                }
+            }
+
+            val durationInMilis = (hoursValue * 60 * 60 * 1000) + currentTimerValueInMilis
+            val timerEndTimeInMilis = currentTime + durationInMilis
+            timerTimeToEnd.text = TimeConverter.formatFromMilliToTime(timerEndTimeInMilis)
+            timerDuration.text = TimeConverter.formatFromMilliToMinutes(durationInMilis.toLong())
+            previousTimerValue = currentTimerValue
+        }
+
+        override fun onStartTrackingTouch(p0: SeekArc?) {
+        }
+
+        override fun onStopTrackingTouch(p0: SeekArc?) {
+        }
+
     }
 }
