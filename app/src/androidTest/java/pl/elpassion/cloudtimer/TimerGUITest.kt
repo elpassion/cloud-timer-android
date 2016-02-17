ebcc152628cc3dadf99d18f2168f9217ca552249
rafal <trzcinskiraf@gmail.com>
2016-02-17 16:51:14 +0100

    refactoring

--- app/src/androidTest/java/pl/elpassion/cloudtimer/TimerGUITest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/TimerGUITest.kt
@@ -1,18 +1,27 @@
 package pl.elpassion.cloudtimer
 
-import android.support.test.espresso.Espresso
-import android.support.test.rule.ActivityTestRule
+import android.support.test.espresso.Espresso.closeSoftKeyboard
 import org.junit.Rule
 import org.junit.Test
+import pl.elpassion.cloudtimer.ComponentsTestsUtils.checkTextMatching
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.isComponentDisplayed
+import pl.elpassion.cloudtimer.ComponentsTestsUtils.typeTextInView
 
 class TimerGUITest {
     @Rule @JvmField
-    val activity = ActivityTestRule<TimerActivity>(TimerActivity::class.java)
+    val rule = rule<TimerActivity> { }
 
     @Test
     fun fifteenMinutesOnStart() {
-        Espresso.closeSoftKeyboard()
+        closeSoftKeyboard()
         isComponentDisplayed(R.id.timer_duration)
+    }
+
+    @Test
+    fun ifTimerIsSetTo25MinutesEndTimeShouldSetProperly() {
+        val currentTime = currentTimeInMillis()
+        val minutes = 25L*60L*1000L
+        typeTextInView(R.id.timer_duration, "25:00")
+        checkTextMatching(R.id.timer_time_to_end, (currentTime + minutes).toString())
     }
 }
--- app/src/main/java/pl/elpassion/cloudtimer/TimerActivity.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/TimerActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.widget.Button
 import android.widget.EditText
@@ -12,7 +13,7 @@ import com.triggertrap.seekarc.SeekArc
 import pl.elpassion.cloudtimer.alarm.scheduleAlarm
 import pl.elpassion.cloudtimer.domain.Timer
 
-class TimerActivity : Activity() {
+class TimerActivity : AppCompatActivity() {
 
     companion object {
         fun start(activity: Activity, timerActivityResultCode: Int) {
@@ -27,6 +28,10 @@ class TimerActivity : Activity() {
     val startTimerButton by lazy { findViewById(R.id.start_timer_button) as Button }
     val timerTitle by lazy { findViewById(R.id.new_timer_title) as EditText }
     val timerToolbar by lazy { findViewById(R.id.new_timer_toolbar) as Toolbar }
+    val beginRange: IntRange = 0..10
+    val endRange: IntRange = 50..59
+    var previousTimerValue: Int = 0
+    var hoursValue: Int = 0
     protected val alarmDao by lazy { TimerDAO.getInstance() }
     var timerDurationInMilis: Long = 0
     private val handler = Handler()
@@ -51,6 +56,7 @@ class TimerActivity : Activity() {
     override fun onResume() {
         handler.postDelayed(TimerEndTimeRefresher(), 1000)
         timerDuration.text = "15:00"
+        refreshTimerEndTime()
         super.onResume()
     }
 
@@ -64,28 +70,36 @@ class TimerActivity : Activity() {
         timerEndTime.text = TimeConverter.formatFromMilliToTime(endTime)
     }
 
-    inner class SeekArcMinutesChangeListener(var previousTimerValue: Int = 0, var hoursValue: Int = 0, private val beginRange: IntRange = 0..10, private val endRange: IntRange = 50..59) : SeekArc.OnSeekArcChangeListener {
+    private fun setTimerHourValue(currentTimerValue: Int) {
+        if (currentTimerValue in beginRange && previousTimerValue.toInt() in endRange) {
+            hoursValue++
+        } else if (previousTimerValue in beginRange && currentTimerValue in endRange) {
+            hoursValue--
+            if (hoursValue < 0) {
+                hoursValue = 0
+            }
+        }
+    }
+
+    private fun onSeekBarTimeChange(currentValue: Int) {
+        val currentTime = System.currentTimeMillis()
+        val currentTimerValue = Math.round(currentValue / 360f * 59)
+        val currentTimerValueInMilis = currentTimerValue * 60L * 1000L
+
+        setTimerHourValue(currentTimerValue)
+
+        timerDurationInMilis = (hoursValue * 60 * 60 * 1000) + currentTimerValueInMilis
+        val timerEndTimeInMilis = currentTime + timerDurationInMilis
+        timerEndTime.text = TimeConverter.formatFromMilliToTime(timerEndTimeInMilis)
+        timerDuration.text = TimeConverter.formatFromMilliToMinutes(timerDurationInMilis.toLong())
+        previousTimerValue = currentTimerValue
+    }
+
+
+    inner class SeekArcMinutesChangeListener : SeekArc.OnSeekArcChangeListener {
 
         override fun onProgressChanged(seekArc: SeekArc?, currentValue: Int, isChanged: Boolean) {
-
-            val currentTime = System.currentTimeMillis()
-            val currentTimerValue = Math.round(currentValue / 360f * 59)
-            val currentTimerValueInMilis = currentTimerValue * 60L * 1000L
-
-            if (currentTimerValue in beginRange && previousTimerValue.toInt() in endRange) {
-                hoursValue++
-            } else if (previousTimerValue in beginRange && currentTimerValue in endRange) {
-                hoursValue--
-                if (hoursValue < 0) {
-                    hoursValue = 0
-                }
-            }
-
-            timerDurationInMilis = (hoursValue * 60 * 60 * 1000) + currentTimerValueInMilis
-            val timerEndTimeInMilis = currentTime + timerDurationInMilis
-            timerEndTime.text = TimeConverter.formatFromMilliToTime(timerEndTimeInMilis)
-            timerDuration.text = TimeConverter.formatFromMilliToMinutes(timerDurationInMilis.toLong())
-            previousTimerValue = currentTimerValue
+            onSeekBarTimeChange(currentValue)
         }
 
         override fun onStartTrackingTouch(seekArc: SeekArc?) {
@@ -93,7 +107,6 @@ class TimerActivity : Activity() {
 
         override fun onStopTrackingTouch(seekArc: SeekArc?) {
         }
-
     }
 
     inner class TimerEndTimeRefresher : Runnable {
