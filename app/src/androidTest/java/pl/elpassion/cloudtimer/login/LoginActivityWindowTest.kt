eeaf2eb46168ce19a494707a6468019bca19546e
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-18 14:25:25 +0100

    tests refactoring

--- app/src/androidTest/java/pl/elpassion/cloudtimer/login/LoginActivityWindowTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/login/LoginActivityWindowTest.kt
@@ -2,7 +2,6 @@ package pl.elpassion.cloudtimer.login
 
 import android.support.test.espresso.Espresso.closeSoftKeyboard
 import android.support.test.runner.AndroidJUnit4
-import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,50 +10,43 @@ import pl.elpassion.cloudtimer.ComponentsTestsUtils.isComponentNotDisplayed
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.pressButton
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.typeText
 import pl.elpassion.cloudtimer.R
-import pl.elpassion.cloudtimer.network.SignInService
-import pl.elpassion.cloudtimer.network.myService
+import pl.elpassion.cloudtimer.TimerDAO
+import pl.elpassion.cloudtimer.domain.Timer
 import pl.elpassion.cloudtimer.rule
-import rx.Observable
+import pl.elpassion.cloudtimer.timerslist.ListOfTimersActivity
 
 @RunWith(AndroidJUnit4::class)
 class LoginActivityWindowTest {
 
     @Rule @JvmField
-    val rule = rule<LoginActivity>(){}
+    val rule = rule<ListOfTimersActivity>() {
+        val alarmDao = TimerDAO.getInstance()
+        alarmDao.deleteAll()
+        alarmDao.save(Timer("timer", 10000))
+    }
 
     @Test
     fun initTest() {
+        pressButton(R.id.timer_share_button)
         isComponentNotDisplayed(R.id.error_message)
     }
 
     @Test
     fun regexNegativeTest() {
-        typeText(R.id.email_input, "potato")
-        closeSoftKeyboard()
-        pressButton(R.id.login_via_email_button)
+        sentActivationEmail("potato")
         isComponentDisplayed(R.id.error_message)
     }
 
     @Test
     fun regexPositiveTest() {
-        typeText(R.id.email_input, "potato@gmail.com")
-        closeSoftKeyboard()
-        pressButton(R.id.login_via_email_button)
-        isComponentNotDisplayed(R.id.error_message)
+        sentActivationEmail("potato@gmail.com")
+        isComponentDisplayed(R.id.create_new_timer)
     }
 
-    @Test
-    fun signInPositiveCaseServiceFired() {
-        var isFired = false
-        myService = object : SignInService {
-            override fun singIn(email: String) : Observable<Any> {
-                isFired = true
-                return Observable.just(Any())
-            }
-        }
-        typeText(R.id.email_input, "potato@gmail.com")
+    private fun sentActivationEmail(email: String) {
+        pressButton(R.id.timer_share_button)
+        typeText(R.id.email_input, email)
         closeSoftKeyboard()
         pressButton(R.id.login_via_email_button)
-        assertTrue(isFired)
     }
 }
