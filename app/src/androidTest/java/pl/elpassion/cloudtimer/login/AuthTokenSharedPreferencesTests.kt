e46d803e83f3a1ef30bbfa36cdcd6e87cd89f5f4
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-19 16:20:50 +0100

    Added first test to AuthTokenSharedPreferences

---
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/login/AuthTokenSharedPreferencesTests.kt
@@ -0,0 +1,14 @@
+package pl.elpassion.cloudtimer.login
+
+import org.junit.Test
+import pl.elpassion.cloudtimer.login.authtoken.AuthTokenSharedPreferences
+
+class AuthTokenSharedPreferencesTests {
+
+
+    @Test
+    fun init(){
+        AuthTokenSharedPreferences()
+    }
+
+}
---
+++ app/src/main/java/pl/elpassion/cloudtimer/login/authtoken/AuthTokenSharedPreferences.kt
@@ -0,0 +1,4 @@
+package pl.elpassion.cloudtimer.login.authtoken
+
+class AuthTokenSharedPreferences {
+}
