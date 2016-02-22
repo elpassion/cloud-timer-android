bbff24dedf44be96c1ba3713d6d1663869086334
rafal <trzcinskiraf@gmail.com>
2016-02-22 15:29:57 +0100

    group color picker #16

--- app/src/main/java/pl/elpassion/cloudtimer/domain/Group.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/domain/Group.kt
@@ -1,5 +1,14 @@
 package pl.elpassion.cloudtimer.domain
 
+import android.graphics.Color
 import java.util.*
 
-class Group(val name: String, val invitationToken: String? = null, val users: MutableList<User> = ArrayList())
+class Group(val name: String, val invitationToken: String? = null, val users: MutableList<User> = ArrayList(), var color: Int = randomColor()) {
+
+    companion object {
+        fun randomColor() : Int {
+            val rnd = Random()
+            return Color.rgb(rnd.nextInt(), rnd.nextInt(), rnd.nextInt())
+        }
+    }
+}
