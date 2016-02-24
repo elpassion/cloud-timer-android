1b5037d7f2d82954a88bfbeb0b8bb8004ef0714d
rafal <trzcinskiraf@gmail.com>
2016-02-24 10:41:05 +0100

    change in random group color #16

--- app/src/main/java/pl/elpassion/cloudtimer/domain/Group.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/domain/Group.kt
@@ -1,14 +1,14 @@
 package pl.elpassion.cloudtimer.domain
 
-import android.graphics.Color
 import java.util.*
 
 class Group(val name: String, val invitationToken: String? = null, val users: MutableList<User> = ArrayList(), var color: Int = randomColor()) {
 
     companion object {
         fun randomColor() : Int {
-            val rnd = Random()
-            return Color.rgb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+            val baseColors = intArrayOf(0xFFFF0000.toInt(), 0xFFFF00FF.toInt(), 0xFF0000FF.toInt(),
+                    0xFF00FFFF.toInt(), 0xFF00FF00.toInt(), 0xFFFFFF00.toInt(), 0xFFFF0000.toInt())
+            return baseColors[Random().nextInt(baseColors.size)]
         }
     }
 }
