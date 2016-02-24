0a4553aa9f022f93ae17f7380ddf4cd8015395a4
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-24 09:28:37 +0100

    - coveralls file changed to work with specific flavor

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./gradlew createToTestsDebugCoverageReport
 mkdir -p app/build/reports/jacoco/test/
-cp app/build/reports/coverage/debug/report.xml app/build/reports/jacoco/test/jacocoTestReport.xml
+cp app/build/reports/coverage/toTests/debug/report.xml app/build/reports/jacoco/test/jacocoTestReport.xml
 ./gradlew coveralls
