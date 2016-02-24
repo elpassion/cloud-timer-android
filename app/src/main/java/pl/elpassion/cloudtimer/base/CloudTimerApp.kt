2f14563a2b87095299e6640cb477755d06f53259
Maciej Górski <maciek.gorski@gmail.com>
2016-02-24 15:33:17 +0100

    Revert "Add leak canary."
    This reverts commit d15e0160b923a2c3693d387f7046e366285b106a.

--- app/src/main/java/pl/elpassion/cloudtimer/base/CloudTimerApp.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/base/CloudTimerApp.kt
@@ -3,7 +3,6 @@ package pl.elpassion.cloudtimer.base
 import android.app.Application
 import android.content.Context
 import com.crashlytics.android.Crashlytics
-import com.squareup.leakcanary.LeakCanary
 import io.fabric.sdk.android.Fabric
 
 
@@ -14,7 +13,6 @@ class CloudTimerApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        LeakCanary.install(this)
         Companion.applicationContext = applicationContext
         Fabric.with(this, Crashlytics())
     }
