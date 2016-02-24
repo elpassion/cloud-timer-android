b809e526f1d3d9637684fa530a028b2f490dcb47
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-24 12:13:39 +0100

    - [#17] tests fix

--- app/src/androidTest/java/pl/elpassion/cloudtimer/login/ActivityStackBehaviourAfterLogin.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/login/ActivityStackBehaviourAfterLogin.kt
@@ -7,6 +7,8 @@ import org.junit.Rule
 import org.junit.Test
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.isComponentDisplayed
 import pl.elpassion.cloudtimer.R
+import pl.elpassion.cloudtimer.TimerDAO
+import pl.elpassion.cloudtimer.domain.Timer
 import pl.elpassion.cloudtimer.login.authtoken.AuthTokenSharedPreferences
 import pl.elpassion.cloudtimer.ruleManuallyStarted
 import rx.Observable
@@ -15,6 +17,7 @@ class ActivityStackBehaviourAfterLogin {
 
     @JvmField @Rule
     val rule = ruleManuallyStarted <LoginActivity> {
+        TimerDAO.getInstance().save(Timer("",100000))
         loginService = object : LoginService {
             override fun login(email: Login): Observable<User> {
                 return Observable.just(User("user", "url", "email", "token"))
