195b2de2de6cbd10d4b2dfebb3afcd9cd7d38e1f
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-24 14:16:49 +0100

    - [#17] tests fix

--- app/src/androidTest/java/pl/elpassion/cloudtimer/signin/LoginActivityStartTest.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/signin/LoginActivityStartTest.kt
@@ -9,6 +9,7 @@ import pl.elpassion.cloudtimer.ComponentsTestsUtils.pressButton
 import pl.elpassion.cloudtimer.R
 import pl.elpassion.cloudtimer.TimerDAO
 import pl.elpassion.cloudtimer.domain.Timer
+import pl.elpassion.cloudtimer.login.authtoken.AuthTokenSharedPreferences
 import pl.elpassion.cloudtimer.rule
 import pl.elpassion.cloudtimer.timerslist.ListOfTimersActivity
 
@@ -19,6 +20,7 @@ class LoginActivityStartTest {
         val alarmDao = TimerDAO.getInstance()
         alarmDao.deleteAll()
         alarmDao.save(Timer("placeholder", 1001000L))
+        AuthTokenSharedPreferences.sharedPreferences.edit().clear().commit()
     }
 
     @Test
