b980f1b445c6013b98e3c468c901801a6d8c118c
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-24 15:15:13 +0100

    - [#17] change to travis and script to run tests only once

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-./gradlew createToTestsDebugCoverageReport
 mkdir -p app/build/reports/jacoco/test/
 cp app/build/reports/coverage/toTests/debug/report.xml app/build/reports/jacoco/test/jacocoTestReport.xml
 ./gradlew coveralls
