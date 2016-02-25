fa114ae903ee0659ca589f581bcad38c7f0c54cc
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-25 14:07:33 +0100

    - [#17] refactoring to tests + tests to check if retryLoggingIn button is not displayed at start, if button is displayed after logging in fail, if button works correctly

--- app/src/androidTest/java/pl/elpassion/cloudtimer/login/ActivityStackBehaviourAfterLogin.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/login/ActivityStackBehaviourAfterLogin.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import android.support.test.espresso.Espresso
 import org.junit.After
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
-import pl.elpassion.cloudtimer.ComponentsTestsUtils
+import pl.elpassion.cloudtimer.ComponentsTestsUtils.checkTextMatching
 import pl.elpassion.cloudtimer.ComponentsTestsUtils.isComponentDisplayed
+import pl.elpassion.cloudtimer.ComponentsTestsUtils.pressButton
 import pl.elpassion.cloudtimer.R
 import pl.elpassion.cloudtimer.TimerDAO
 import pl.elpassion.cloudtimer.domain.Timer
@@ -23,6 +25,22 @@ class ActivityStackBehaviourAfterLogin {
         AuthTokenSharedPreferences.sharedPreferences.edit().clear().commit()
     }
 
+    var serviceCallCounter  = 0
+
+    val loginServiceSuccess: LoginService = object : LoginService {
+        override fun login(email: Login): Observable<User> {
+            ++serviceCallCounter
+            return Observable.just(User("user", "url", "email", "token"))
+        }
+    }
+
+    val loginServiceFail: LoginService = object : LoginService {
+        override fun login(email: Login): Observable<User> {
+            ++serviceCallCounter
+            return Observable.error(Throwable())
+        }
+    }
+
     @After
     fun closePreviousActivities(){
         try {
@@ -32,28 +50,33 @@ class ActivityStackBehaviourAfterLogin {
 
     @Test
     fun whenStackOfActivitiesIsEmptyLoginActivityShouldStartListOfTimersActivity() {
-        loginService = object : LoginService {
-            override fun login(email: Login): Observable<User> {
-                return Observable.just(User("user", "url", "email", "token"))
-            }
-        }
-        val intent = Intent()
-        intent.data = Uri.parse("test")
-        rule.launchActivity(intent)
+        loginService = loginServiceSuccess
+        launchActivityWithData()
         isComponentDisplayed(R.id.create_new_timer)
     }
 
     @Test
-    fun whenLoginApiFailedThereShouldBeMessageErrorShowed() {
-        loginService = object : LoginService {
-            override fun login(email: Login): Observable<User> {
-                return Observable.error(Throwable())
-            }
-        }
+    fun whenLoginApiFailedThereShouldBeMessageErrorAndRetryLoggingInButtonShowed() {
+        loginService = loginServiceFail
+        launchActivityWithData()
+        checkTextMatching(R.id.logging_message, R.string.logging_in_failure)
+        isComponentDisplayed(R.id.retry_logging_in)
+    }
+
+    @Test
+    fun ifUserHasTriedLogInAgainThereShouldBeTwoCallsToApi() {
+        loginService = loginServiceFail
+        launchActivityWithData()
+        loginService = loginServiceSuccess
+        pressButton(R.id.retry_logging_in)
+        Assert.assertEquals(2, serviceCallCounter)
+    }
+
+    private fun launchActivityWithData() {
         val intent = Intent()
         intent.data = Uri.parse("test")
         rule.launchActivity(intent)
-        ComponentsTestsUtils.checkTextMatching(R.id.logging_message, R.string.logging_in_failure)
     }
+
 
 }
