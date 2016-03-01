4d272f795dd99fa689a55fba4ff9fcc129bc2616
dmalantowicz <dominik.malantowicz@gmail.com>
2016-03-01 13:48:38 +0100

    -tests refactoring

--- app/src/androidTest/java/pl/elpassion/cloudtimer/dao/RealmTimerDaoTests.kt
+++ app/src/androidTest/java/pl/elpassion/cloudtimer/dao/RealmTimerDaoTests.kt
@@ -3,6 +3,7 @@ package pl.elpassion.cloudtimer.dao
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import pl.elpassion.cloudtimer.domain.Group
 import pl.elpassion.cloudtimer.domain.Timer
 
 class RealmTimerDaoTests() {
@@ -27,7 +28,7 @@ class RealmTimerDaoTests() {
 
     @Test
     fun findOneShouldReturnCorrectObject() {
-        val firstTimer = Timer("timer1", 200)
+        val firstTimer = Timer("timer1", 200, 200, "123", Group("name", 1), true)
         dao.save(firstTimer)
         val timerFromDb = dao.findOne(firstTimer.uid)
         assertEquals(firstTimer, timerFromDb)
--- app/src/main/java/pl/elpassion/cloudtimer/dao/RealmTimerDao.kt
+++ app/src/main/java/pl/elpassion/cloudtimer/dao/RealmTimerDao.kt
@@ -2,55 +2,52 @@ package pl.elpassion.cloudtimer.dao
 
 import io.realm.Realm
 import pl.elpassion.cloudtimer.domain.Timer
-import java.util.*
 
 object RealmTimerDao : TimerDao {
 
-    private fun Realm.inTransaction(inTransaction: Realm.() -> Unit) {
+    private fun <T> Realm.inTransaction(inTransaction: Realm.() -> T): T {
         beginTransaction()
-        inTransaction()
+        val funcResult = inTransaction()
         commitTransaction()
+        return funcResult
     }
 
-    private fun inRealm(inRealm: Realm.() -> Unit) {
-        val realm = Realm.getDefaultInstance();
-        inRealm(realm)
-        realm.close();
+    private fun <T> inRealm(inRealm: Realm.() -> T): T {
+        val realm = Realm.getDefaultInstance()
+        try {
+            return inRealm(realm)
+        } finally{
+            realm.close()
+        }
     }
 
     override fun save(timer: Timer): String {
-        inRealm {
+        return inRealm {
             inTransaction {
-                copyToRealmOrUpdate(TimerRealmObject(timer))
+                copyToRealmOrUpdate(TimerRealmObject(timer)).uuid
             }
         }
-        return timer.uid
     }
 
     override fun findAll(): List<Timer> {
-        val timers: MutableList<Timer> = ArrayList()
-        inRealm {
-            val list = where(TimerRealmObject::class.java)
+        return inRealm {
+            where(TimerRealmObject::class.java)
                     .findAll().map { it.createTimer() }
-            timers.addAll(list)
         }
-        return timers
     }
 
     override fun findOne(uuId: String): Timer? {
-        var timer : Timer? = null
-        inRealm {
-            timer = where(TimerRealmObject::class.java)
+        return inRealm {
+            where(TimerRealmObject::class.java)
                     .equalTo("uuid", uuId).findFirst().createTimer()
         }
-        return timer
     }
 
     override fun deleteAll() {
         inRealm {
             inTransaction {
                 where(TimerRealmObject::class.java)
-                .findAll().removeAll { true }
+                        .findAll().removeAll { true }
             }
         }
     }
